01b6191d224d1ae20cc9a24de6dc9b3328d9ce88
anil <alianilkocak@gmail.com>
2020-11-29 01:14:53 -0800

    rate limit artıtıldı

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'throttle:240,1',
             SubstituteBindings::class,
         ],
     ];
